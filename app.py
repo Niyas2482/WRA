9fc39e0a23b7006b5a55e486db5f57b92be9f451
A. Mohammed Niyas <mohammedniyassafri@gmail.com>
2026-04-20 22:38:59 +0530

    removed the old config

--- app.py
+++ app.py
@@ -915,6 +915,5 @@ if __name__ == '__main__':
         seed_database()
     app.run(debug=False, host='0.0.0.0', port=5000)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+
   
